508edb54d8a5c49b02b157d54c1f5b93b0d65a0f
Ahmedigit <meetahmedifatima@gmail.com>
2020-09-11 16:37:46 -0400

    first commit

---
+++ src/test/java/qaclick/Mavenjava/AbcTest.java
@@ -0,0 +1,16 @@
+package qaclick.Mavenjava;
+import org.testng.annotations.Test;
+
+
+public class AbcTest {
+
+		// TODO Auto-generated method stub
+	
+	@Test 
+	public void Demo()
+	{
+		System.out.println("motu");
+
+	}
+
+}
---
+++ src/test/java/qaclick/Mavenjava/BcdTest.java
@@ -0,0 +1,12 @@
+package qaclick.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class BcdTest {
+
+	@Test 
+	public void Abd()
+	{
+		System.out.println("motu");
+	 }
+}
